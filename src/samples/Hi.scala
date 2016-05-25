d83d6fe270288c98812df145f8d086b38df8dec2
t_ishikawa <tissi0708@gmail.com>
2016-05-25 09:44:55 +0900

    add readme

--- src/samples/Hi.scala
+++ src/samples/Hi.scala
@@ -2,4 +2,14 @@ package samples
 
 object Hi {
   def main(args: Array[String]) = println("Hi!")
+
+  val name: Option[String] = Some("ishi")
+
+  val realName : String = name match {
+    case Some("taka") => "first-name"
+    case Some("") => "nothing"
+    case Some(name) => name
+  }
+
+  println(realName)
 }
